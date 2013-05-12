73fdf38edcdebf4d2ace7f962677312c8d4f0f9d
Marcin Grzywaczewski <killavus@gmail.com>
2013-05-12 17:20:49 +0200

    Added jQuery and Twitter Bootstrap

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="pl">
+  <head>
+    <title>BD2013 &ndash; Gry Tekstowe</title>
+    <meta charset="UTF-8" />
+
+    <link rel="stylesheet" href="css/bootstrap.css" />
+    <link rel="stylesheet" href="css/boostrap-responsive.css" />
+    <link rel="stylesheet" href="css/main.css" />
+
+    <script src="js/bootstrap.js"></script>
+    <script src="js/jquery.js"></script>
+  </head>
+  <body>
+    <h1>WITAMY</h1>
+  </body>
+</html>
+  
