6f0d84e614389883a691f2fcdf8e3c5e73aaafbd
Marcin Grzywaczewski <killavus@gmail.com>
2013-06-10 00:13:13 +0000

    Poprawione logowanie - przeczytaj Jadziu :)

--- actions/login.php
+++ actions/login.php
@@ -5,8 +5,15 @@ require_once 'src/core.php';
 if(!isSet($_POST['login']) or !isSet($_POST['password']))
 	die("Brak danych w formularzu.");
 
-$user = mysql_real_escape_string($_POST['login']);
-$passwd = mysql_real_escape_string($_POST['password']);
+$db = user_database();
+
+/* To tak: (pamiętajmy, że pracujemy w PDO, nie używamy funkcji z rodziny mysql_
+   To przestarzałe.
+   Taki jest sposób na escaping w PDO:
+$user = $db->quote($_POST['login']);
+$passwd = $db->quote($_POST['password']);
+Ale to jest niepotrzebne, bo w sign_in są zapytania przygotowane i to się samo escapuje.
+*/
 
 if(preg_match('/^[a-zA-Z0-9_\-\.]{3,}$/', $user) !== 1) {
 	redirect_to('login',['error' => 1]);
