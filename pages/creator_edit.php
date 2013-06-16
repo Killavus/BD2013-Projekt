572473545378224f9dd0bb907613a7c6802cff82
jandryszak <jadwiga.andryszak@gmail.com>
2013-06-17 00:13:21 +0200

    Poprawiłam stylowanie jednego formularza (zepsute przypadkiem)

--- pages/creator_edit.php
+++ pages/creator_edit.php
@@ -51,7 +51,7 @@ function message() {
       <p><a href="#" id="set_primary_button" class="btn btn-primary">Ustal pytanie startowe</a>
 			lub <a href="?page=creator">powróć na stronę główną</a>.</p>
 			<div id="set_primary" style="display:none">
-				<div style="margin: 10px; padding: 20px; border: 1px solid skyblue; -moz-border-radius: 10px; border-radius: 10px; -webkit-border-radius: 10px">
+				<div style="margin: 5px; padding: 10px; border: 1px solid skyblue; -moz-border-radius: 10px; border-radius: 10px; -webkit-border-radius: 10px">
 					<form class="form-inline" action="actions/game_set_primary.php?gid=<?php echo $game_id; ?>" method="post">
 						<select name="primary">
 						<?php
@@ -161,7 +161,7 @@ function message() {
       <h2>Odpowiedzi:</h2>
 			<button id="add_answer_button" class="btn btn-success" type="button"> Stwórz odpowiedź </button>
 			<div id="add_answer" style="display: none">
-				<div style="border: 1px solid skyblue; -moz-border-radius: 10px; border-radius: 10px; -webkit-border-radius: 10px">
+				<div style="margin: 10px; padding: 20px; border: 1px solid skyblue; -moz-border-radius: 10px; border-radius: 10px; -webkit-border-radius: 10px">
 					<form action="actions/add_answer.php?gid=<?php echo $game_id; ?>" method="post" class="form-horizontal">
 						<div class="control-group">
 							<label class="control-label" for="ans_nazwa"> Nazwa: </label>
